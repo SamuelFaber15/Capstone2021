1050c798b15fcdca6a082982b0d865a942c38513
Samuel Faber <samuel.w.faber@gmail.com>
2021-01-22 16:11:19 -0700

    Initial Commit

---
+++ Capstone_Jan_22/Capstone_Jan_22App.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_Jan_22App.swift
+//  Capstone_Jan_22
+//
+//  Created by Samuel Faber on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_Jan_22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capstone_Jan_22/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Capstone_Jan_22
+//
+//  Created by Samuel Faber on 1/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
