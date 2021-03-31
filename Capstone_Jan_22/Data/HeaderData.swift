c87761274cdcb8724e9bcca48c880c949fd446b8
Ashleigh Koener <akoener@halosight.com>
2021-03-31 14:12:13 -0600

    new potential app icon

--- Capstone_Jan_22/Data/HeaderData.swift
+++ Capstone_Jan_22/Data/HeaderData.swift
@@ -56,7 +56,7 @@ let headersData: [Header] = [
     Header(
       image: "try",
       headline: """
-      "Mental health problems don’t define who you are. They are something you experience. You walk in the rain and you feel the rain, but... YOU ARE NOT THE RAIN"
+      "Mental health problems don’t define who you are. They are something you experience. You walk in the rain and you feel the rain, but, importantly, YOU ARE NOT THE RAIN"
       """,
       subheadline: "    ~ Matt Haig"
     )
